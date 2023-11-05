8509f4860aa318445f567d57b158e6da6f289dd4
LeoAzevedo59 <leofrancoazevedo@gmail.com>
2023-11-04 22:55:58 -0300

    feat: adicionado support elastic search kibana

--- devops-api/devops-api/Program.cs
+++ devops-api/devops-api/Program.cs
@@ -1,7 +1,9 @@
 using Dapper;
 using System.Data.Common;
 using System.Data.SqlClient;
-using System.Linq;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +23,41 @@ builder.Services.AddCors(options =>
       });
 });
 
+//configure logging first
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile(
+        $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+        optional: true)
+    .Build();
+
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .Enrich.WithMachineName()
+    .WriteTo.Debug()
+    .WriteTo.Console()
+    .WriteTo.Elasticsearch(ConfigureElasticSink(configuration, environment))
+    .Enrich.WithProperty("Environment", environment)
+    .ReadFrom.Configuration(configuration)
+    .CreateLogger();
+
+static ElasticsearchSinkOptions ConfigureElasticSink(IConfigurationRoot configuration, string environment)
+{
+    string IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}";
+
+    return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
+    {
+        AutoRegisterTemplate = true,
+        IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+    };
+}
+
+// Add configuration sources
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true);
+
 var connection = "Server=devops-db-sql-server;DataBase=master;Uid=sa;Pwd=devops-azevedo#2023;";
 builder.Services.AddScoped<DbConnection>(e => new SqlConnection(connection));
 
@@ -29,11 +66,8 @@ builder.Services.AddScoped<DbConnection>(e => new SqlConnection(connection));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
@@ -78,10 +112,12 @@ app.MapGet("/todos", async (DbConnection db) =>
     try
     {
         var todos = (await db.QueryAsync<Todo>(query)).ToList();
+        Log.Information("request GET: /todos success");
         return todos;
     }
     catch (Exception ex)
     {
+        Log.Error("request GET: /todos error {ex}", ex.Message);
         throw;
     }
     finally
@@ -95,42 +131,93 @@ app.MapPost("/todos", async (Todo todo, DbConnection db) =>
 {
     int _checked = (todo.isChecked == true ? 1 : 0);
 
-    await db.OpenAsync();
+    try
+    {
+        await db.OpenAsync();
 
-    string query = @$"INSERT INTO todos(id, description, isChecked)
+        string query = @$"INSERT INTO todos(id, description, isChecked)
                           values('{todo.Id}', '{todo.Description}', {_checked})";
 
-    await db.ExecuteAsync(query);
-
-    await db.CloseAsync();
+        await db.ExecuteAsync(query);
+        Log.Information("request POST: /todos success");
+    }
+    catch (Exception ex)
+    {
+        Log.Error("request POST: /todos error {ex}", ex.Message);
+        throw;
+    }
+    finally
+    {
+        await db.CloseAsync();
+    }
 
 }).WithName("todos_post");
 
 app.MapDelete("/todos/{id}", async (string id, DbConnection db) =>
 {
-    await db.OpenAsync();
+    try
+    {
+        await db.OpenAsync();
 
-    string query = @$"delete from todos
+        string query = @$"delete from todos
                       where id = '{id}'";
 
-    await db.ExecuteAsync(query);
-
-    await db.CloseAsync();
+        await db.ExecuteAsync(query);
+        Log.Information("request DELETE: /todos success");
+    }
+    catch (Exception ex)
+    {
+        Log.Error("request DELETE: /todos error {ex}", ex.Message);
+        throw;
+    }
+    finally
+    {
+        await db.CloseAsync();
+    }
 }).WithName("todos_delete");
 
 app.MapPut("/todos/{id}", async (string id, bool isChecked, DbConnection db) =>
 {
     int _checked = (isChecked == true ? 1 : 0);
 
-    await db.OpenAsync();
-    string query = @$"update todos
+    try
+    {
+        await db.OpenAsync();
+        string query = @$"update todos
                       set isChecked = {_checked}
                       where id = '{id}'";
-    await db.ExecuteAsync(query);
-
-    await db.CloseAsync();
+        await db.ExecuteAsync(query);
+        Log.Information("request PUT: /todos success");
+    }
+    catch (Exception ex)
+    {
+        Log.Error("request PUT: /todos error {ex}", ex.Message);
+        throw;
+    }
+    finally
+    {
+        await db.CloseAsync();
+    }
 
 }).WithName("todos_put");
+
+#region ERROS FORCE
+
+app.MapGet("/notFound", async (DbConnection db) =>
+{
+    Log.Error("request GET: /notFound");
+    return Results.NotFound();
+
+}).WithName("not_found");
+
+app.MapGet("/Unauthorized", async (DbConnection db) =>
+{
+    Log.Error("request GET: /Unauthorized");
+    return Results.Unauthorized();
+
+}).WithName("unauthorized");
+
+#endregion
 
 app.UseCors();
 app.Run();
